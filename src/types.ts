e33e9d6257d5f28523631ecefbc7baacd6d6d288
Leonardo <leoomorais@hotmail.com>
2026-09-08 16:51:57 -0300

    Add channel quality variants type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
+export type ChannelVariant = {
+  id: string
+  name: string
+  url: string
+  group: string
+  logo?: string
+  tvgId?: string
+  tvgName?: string
+  quality: string
+}
+
 export type Channel = {
   id: string
   name: string
@@ -6,6 +17,7 @@ export type Channel = {
   logo?: string
   tvgId?: string
   tvgName?: string
+  variants?: ChannelVariant[]
 }
 
 export type Playlist = {
